c2f414184dc694bb05e0fadf7b293f3ccb67570b
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2023-09-18 23:56:14 +0700

    another navbar design update

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ import NavLink from "./NavLink";
 import MenuOverlay from "./MenuOverlay";
 import {styles} from "../styles"
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
-import AboutImage from "@/src/logo_h.png";
+import logoImage from "@/src/logo_h.png";
 
 const navLinks = [
     {
@@ -31,11 +31,9 @@ const Navbar = () => {
             <div className='lg:max-w-[1400px] max-w-screen-lg flex justify-between items-center mx-auto'>
                 <Link href={"/"} className='text-5xl text-white font-semibold'>
                     <Image
-                        src={AboutImage}
+                        src={logoImage}
                         alt='AboutImage'
-                        width={125}
-                        height={38.5}
-                        className='rounded-[30px]'
+                        className='w-[125px] h-[38.5px]'
                     />
                 </Link>
                 <div className='mobile-menu block md:hidden'>
--- app/utils/motion.js
+++ app/utils/motion.js
@@ -16,22 +16,22 @@ export const textVariant = (delay) => {
     };
 };
 
-export const fadeIn = (direction, type, delay, duration) => {
+export const fadeIn = (direction, delay) => {
     return {
         hidden: {
-            x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+            y: direction === 'up' ? 80 : direction === 'down' ? -80 : 0,
             opacity: 0,
+            x: direction === 'left' ? 80 : direction === 'right' ? -80 : 0,
         },
         show: {
-            x: 0,
             y: 0,
+            x: 0,
             opacity: 1,
             transition: {
-                type: type,
+                type: 'tween',
+                duration: 1.2,
                 delay: delay,
-                duration: duration,
-                ease: "easeOut",
+                ease: [0.25, 0.25, 0.25, 0.75],
             },
         },
     };
