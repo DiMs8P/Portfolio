76438f250130920e15a298caafb43fb395d3b88d
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2023-09-24 21:17:53 +0700

    footer translated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Image from "next/image";
+import {styles} from "../[locale]/styles"
+import logoImage from "@/src/logo_h.png";
 
 const Footer = () => {
   return (
-    <footer className='footer border z-10 border-t-[#33353F]
-    border-l-transparent border-r-transparent border-b-transparent text-white'>
-      <div className='container p-12 flex justify-between'>
-        <span>
-          LOGO
-        </span>
+    <footer className={`${ styles.paddingX } footer border-t z-10 border-t-[#33353F] text-white`}>
+      <div className='lg:max-w-[1400px] max-w-screen-lg flex justify-between items-center mx-auto gap-x-12 py-12'>
+          <Image
+              src={logoImage}
+              alt='AboutImage'
+              className='w-[125px] h-[38.5px]'
+          />
         <p className='text-slate-600'>
-            All rights reserved.
+            <a href="https://www.flaticon.com/free-icons/flag" title="idea icons">Flag icons created by Freepik - Flaticon</a>
         </p>
       </div>
     </footer>
