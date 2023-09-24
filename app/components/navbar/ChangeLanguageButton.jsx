c9b5b86158f79aa42aa68062955ae4620400b618
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2023-09-24 21:59:24 +0700

    attempt to fix routing bug

--- app/components/navbar/ChangeLanguageButton.jsx
+++ app/components/navbar/ChangeLanguageButton.jsx
@@ -1,25 +1,19 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
-import {useRouter} from "next-intl/client";
 import {useLocale} from "next-intl";
 import {LANGUAGE_DATA} from "./constants";
 
 const ChangeLanguageButton = () => {
-    const router = useRouter();
     const locale = useLocale();
+    const newPath = locale === "en" ? "/".concat("ru") : "/".concat("en")
 
   return (
-      <Link href='/'>
+      <Link href={newPath}>
           <Image
               src={LANGUAGE_DATA.find((t) => t.id === locale).img}
               alt='AboutImage'
               className='w-[30px] h-[30px]'
-              onClick={() => {
-                  locale === "en" ?
-                      router.replace('/ru', {locale: 'ru'}) :
-                      router.replace('/en', {locale: 'en'})
-              }}
           />
       </Link>
   )
