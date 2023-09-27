389160da9fa45067855538e39272d0f83d727fd1
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2023-09-27 19:56:20 +0700

    fix prject card fitness

--- app/components/projects/ProjectCard.jsx
+++ app/components/projects/ProjectCard.jsx
@@ -20,7 +20,7 @@ const ProjectCard = ({
                     scale: 1,
                     speed: 450,
                 }}
-                className='bg-tertiary p-5 rounded-2xl md:w-[360px] w-full'
+                className='bg-tertiary p-5 rounded-2xl w-full'
             >
                 <div className='relative w-full h-[230px]'>
                     <Image
