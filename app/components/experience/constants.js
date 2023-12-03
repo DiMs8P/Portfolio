09c89ba91f7ab2ba8c4db91023524657b54cc723
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2023-12-03 21:26:56 +0700

    added experience text

--- app/components/experience/constants.js
+++ app/components/experience/constants.js
@@ -1,18 +1,17 @@
-import GithubIcon from '../../../src/github-icon.svg'
+import DreamVRIcon from '../../../src/DreamVR.jpg'
 
 const experiences = (t) => {
     return [
         {
-            title: "React.js Developer",
+            title: "Unreal Engine Developer",
             company_name: "DreamVR",
-            icon: GithubIcon,
+            icon: DreamVRIcon,
             iconBg: "#383E56",
             date: t("DreamVR.Date"),
             points: [
                 t("DreamVR.Points.FirstPoint"),
                 t("DreamVR.Points.SecondPoint"),
-                t("DreamVR.Points.ThirdPoint"),
-                t("DreamVR.Points.ForthPoint")
+                t("DreamVR.Points.ThirdPoint")
             ],
         },
     ];
